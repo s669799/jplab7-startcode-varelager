ae69f07788f696277e88c3d64bc2b1dd725ff12b
Severin <Severin@10.0.0.40>
2022-10-09 19:38:45 +0200

    a done

--- startcode/src/no/hvl/dat100/varelager/Varelager.java
+++ startcode/src/no/hvl/dat100/varelager/Varelager.java
@@ -4,43 +4,50 @@ public class Varelager {
 
 	protected Vare[] varer;
 	protected int antall;
-	
+
 	public Varelager(int n) {
-		
-		throw new TODO("Varelager");
+
+		varer = new Vare[n];
+		antall = 0;
 
 	}
-	
+
 	public Vare[] getVarer() {
-		
-		throw new TODO("getVarer");
+
+		return varer;
 
 	}
-	
+
 	public boolean leggTilVare(Vare v) {
 		
-		throw new TODO("leggTilVare");
+		boolean lagtTil = false;
+		if (antall < varer.length) {
+			varer[antall + 1] = v;
+			lagtTil = true;
+		}
+		antall++;
+		return lagtTil;
 
 	}
-	
+
 	public boolean leggTil(int varenr, String navn, double pris) {
-		
+
 		throw new TODO("leggTil");
 
 	}
-	
+
 	public Vare finnVare(int varenr) {
-			
+
 		throw new TODO("finnVare");
 
 	}
-	
+
 	private String SEP = "==============================";
-	
+
 	public void printVarelager() {
-				
+
 		throw new TODO("printVarelager");
 
 	}
-	
+
 }
