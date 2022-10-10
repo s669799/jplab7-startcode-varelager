ed8b6eef9a20600a3d6add152707311263892fad
Severin <Severin@10.0.0.40>
2022-10-10 20:57:09 +0200

    B2 done

--- startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
+++ startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
@@ -3,26 +3,46 @@ package no.hvl.dat100.varelager;
 public class VarelagerUtils {
 
 	public static Vare finnBilligste(Vare[] varer) {
-		
-		throw new TODO("finnBilligste");
 
-	}
-	
-	public static double totalPris(Vare[] varer) {		
-		
-		throw new TODO("totalPris");
+		double billig = Double.MAX_VALUE;
+		Vare billigst = null;
 
+		for (int i = 0; i < varer.length; i++) {
+			if (billig > varer[i].getPris()) {
+				billig = varer[i].getPris();
+				billigst = varer[i];
+			}	
+		}
+		return billigst;
 	}
-	
+
+	public static double totalPris(Vare[] varer) {
+
+		double sum = 0.0;
+		
+		for (int i = 0; i < varer.length; i++ ) {
+			sum += varer[i].getPris();
+		}
+		return sum;
+	}
+
 	public static int[] finnVarenr(Vare[] varer) {
-				
-		throw new TODO("finnVarenr");
 
+		int[] varenr = new int[varer.length];
+		
+		for (int i = 0; i < varenr.length; i++) {
+			varenr[i] = varer[i].getVarenr();
+		}
+		return varenr;
 	}
-	
-	public static double[] finnPrisDifferanser(Vare[] varer) {
-				
-		throw new TODO("finnPrisDifferanser");
 
+	public static double[] finnPrisDifferanser(Vare[] varer) {
+
+		double[] prisDiff = new double[varer.length-1];
+		
+		for (int i = 0; i < prisDiff.length; i++) {
+			prisDiff[i] = varer[i+1].getPris() - varer[i].getPris();	
+		}
+		return prisDiff;
 	}
 }
